3e74334acf3d4708549a47a5d3b27414734eea6a
Gildinei S. Anunciação <gildineisa@gmail.com>
2021-09-23 21:44:05 -0300

    Add excercicies about console.log

--- 001-console-log/index.js
+++ 001-console-log/index.js
@@ -1,3 +1,5 @@
 console.log("Gildinei Anunciação");
 console.log('Gildinei S. Anunciação');
 console.log(`Gildinei Santos da Anunciação`);
+
+console.log('Meu nome é "Gildinei". Estou aprendendo JavaScript às', 21, 'da noite.');
